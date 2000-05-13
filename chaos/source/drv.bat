5a2343b9e4846b11777afc7a1f85fa3706cc6f2b
Evgeny Gryaznov <egryaznov@gmail.com>
2000-05-13 21:51:24 +0300

    chaos: rev 100, May 13, 2000

---
+++ chaos/source/drv.bat
@@ -0,0 +1,17 @@
+@echo off
+
+cd ..
+
+echo.
+echo  -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+echo.
+echo Building drivers ...
+echo.
+F:\SOFT\CHASM\chasm_w.exe source\drv\keyb.csm
+echo.
+F:\SOFT\CHASM\chasm_w.exe source\drv\display.csm
+echo.
+F:\SOFT\CHASM\chasm_w.exe source\drv\startup.csm
+echo.
+F:\SOFT\CHASM\chasm_w.exe source\drv\fsys.csm
+echo.
---
+++ chaos/source/kern.bat
@@ -0,0 +1,26 @@
+ : ChaOS kernel makefile :
+
+@echo off
+
+cd ..
+
+echo.
+echo  -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+echo.
+echo Building kernel loader ...
+echo.
+F:\SOFT\CHASM\chasm_w.exe source\kernel\chaos.csm
+
+echo.
+echo Building kernel ...
+echo.
+F:\SOFT\CHASM\chasm_w.exe source\kernel\kernel.csm
+
+echo.
+choice Run ChaOS now
+if errorlevel 2 goto no_chaos
+echo.
+chaos
+echo.
+:no_chaos
+echo.
